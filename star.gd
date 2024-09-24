a140654a59e1eacfdf99ae68c9e6c817791e30e9
Nyonyix <quontex@hotmail.com>
2024-09-24 12:45:30 +1000

    Hi

--- star.gd
+++ star.gd
@@ -1,5 +1,36 @@
 class_name Star extends Node2D
 
-    func _init() -> void:
+var spectral_class: String:
+    get:
+        return spectral_class
+    set(value):
+        spectral_class = value
+
+var temperature: int:
+    get:
+        return temperature
+    set(value):
+        temperature = value
+        
+var mass: float:
+    get:
+        return mass
+    set(value):
+        mass = value
+
+var radius: float:
+    get:
+        return radius
+    set(value):
+        radius = value
+
+var luminosity: float:
+    get:
+        return luminosity
+    set(value):
+        luminosity = value
+
+
+func _init() -> void:
 
         pass
