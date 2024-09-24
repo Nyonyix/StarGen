34d832401b171880f051fac51344784d014570ad
Nyonyix <quontex@hotmail.com>
2024-09-24 18:40:22 +1000

    Hi 3

--- main.gd
+++ main.gd
@@ -4,11 +4,14 @@ extends Node
 func _ready() -> void:
 
 	var def_file = FileAccess.open("res://definitions.json", FileAccess.READ).get_as_text()
-	if JSON.parse_string(def_file) == OK:
-		var def_data = 
+	var def_data = JSON.parse_string(def_file)
+	var star_seed = 167333
 
+	for i in range(10):
 
-	var star = Star.new()
+		var star = Star.new(def_data["star"], star_seed)
+		print(star.get("stellar_class_string"))
+		star_seed += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- star.gd
+++ star.gd
@@ -1,36 +1,98 @@
 class_name Star extends Node2D
 
-var spectral_class: String:
-    get:
-        return spectral_class
-    set(value):
-        spectral_class = value
+var def_data: Dictionary
+var rng: RandomNumberGenerator
+
+var scaler: float:
+	get:
+		return scaler
+	set(value):
+		scaler = value
+
+var spectral_letter: String:
+	get:
+		return spectral_letter
+	set(value):
+		spectral_letter = value
+
+var spectral_descriptor: String:
+	get:
+		return spectral_descriptor
+	set(value):
+		spectral_descriptor = value
+
+var luminosity_descriptor: String:
+	get:
+		return luminosity_descriptor
+	set(value):
+		luminosity_descriptor = value
+
+var stellar_class_string: String:
+	get:
+		return stellar_class_string
+	set(value):
+		stellar_class_string = value
 
 var temperature: int:
-    get:
-        return temperature
-    set(value):
-        temperature = value
-        
+	get:
+		return temperature
+	set(value):
+		temperature = value
+
 var mass: float:
-    get:
-        return mass
-    set(value):
-        mass = value
+	get:
+		return mass
+	set(value):
+		mass = value
 
 var radius: float:
-    get:
-        return radius
-    set(value):
-        radius = value
+	get:
+		return radius
+	set(value):
+		radius = value
 
 var luminosity: float:
-    get:
-        return luminosity
-    set(value):
-        luminosity = value
+	get:
+		return luminosity
+	set(value):
+		luminosity = value
 
+func _init(def_input: Dictionary, star_seed: int = 1) -> void:
 
-func _init() -> void:
+	def_data = def_input
+	rng = RandomNumberGenerator.new()
+	rng.seed = star_seed
 
-        print("Hello")
+	scaler = 10 - rng.randf_range(0, 10)
+	_generate_spectral_class()
+	stellar_class_string = "%s V%d %s Main Sequence" % [spectral_letter, scaler, spectral_descriptor]
+
+func _generate_spectral_class():
+
+	var spectral_classes_data = def_data["spectral_classes"]
+	var spectral_classes: Dictionary
+	var weight_check = 0
+	var current_weight = 0
+	var random_weight = rng.randf_range(0, 100)
+
+	for k in spectral_classes_data.keys():
+
+		var weight = spectral_classes_data[k]["weight"]
+		spectral_classes[k] = weight
+
+		weight_check += weight
+		if weight_check > 100:
+			print("Spectral Class Weight > 100")
+	if weight_check < 100:
+		print("Spectral Class Weight < 100")
+
+	for k in spectral_classes.keys():
+
+		breakpoint
+
+		var weight = spectral_classes[k]
+		current_weight += weight
+
+		if random_weight <= current_weight:
+			spectral_letter = k
+			spectral_descriptor = spectral_classes_data[k]["spectral_class_descriptor"]
